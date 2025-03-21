f280f5dc23532b5ad0f98e98b58dde3cab78bfb2
Mloan <loan.mata@ynov.com>
2025-03-21 16:53:13 +0100

    Feat(client/gui): adding the mainFrame class

---
+++ vpnClient/src/fr/ynov/vpnClient/gui/MainFrame.java
@@ -0,0 +1,38 @@
+package fr.ynov.vpnClient.gui;
+
+import fr.ynov.vpnClient.model.ClientSocket;
+
+import javax.swing.*;
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainFrame extends JFrame {
+    private CardLayout cl;
+    private JPanel mainPanel;
+
+    private List<ClientSocket> csList = new ArrayList<>();
+    public MainFrame() {
+        super();
+        init();
+    }
+    public MainFrame(String title) {
+        super(title);
+        init();
+    }
+
+    private void init() {
+
+        getContentPane().removeAll();
+        cl = new CardLayout();
+        mainPanel = new JPanel(cl);
+
+        setSize(800, 600);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        add(mainPanel);
+    }
+
+
+}
