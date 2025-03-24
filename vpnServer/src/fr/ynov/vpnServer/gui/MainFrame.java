c00bbbf68fd6ab8eb5de3a80ebfee1049e9028cc
Mloan <loan.mata@ynov.com>
2025-03-24 15:23:33 +0100

    Feat(server): setting up MainFrame class for server gui

---
+++ vpnServer/src/fr/ynov/vpnServer/gui/MainFrame.java
@@ -0,0 +1,48 @@
+package fr.ynov.vpnServer.gui;
+
+import javax.swing.*;
+import java.awt.*;
+import java.net.ServerSocket;
+
+public class MainFrame  extends JFrame {
+    private ServerSocket ss = null;
+    private final SetupPanel sp = new SetupPanel(this);
+    private final String title;
+    private CardLayout cl;
+    private JPanel mainPanel;
+
+    public MainFrame() {
+        super();
+        this.title = "Server app";
+        setTitle(title);
+        init();
+    }
+    public MainFrame(String title) {
+        super(title);
+        this.title = title;
+        init();
+    }
+
+    private void init() {
+        setTitle(title);
+
+        getContentPane().removeAll();
+        cl = new CardLayout();
+        mainPanel = new JPanel(cl);
+
+        setSize(800, 600);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        if(ss == null) {
+            showSetupPanel();
+        }
+        add(mainPanel);
+    }
+
+    private void showSetupPanel() {
+        mainPanel.add(sp, "setup");
+    }
+
+
+}
